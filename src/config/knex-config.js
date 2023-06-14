966856e63997eb9fd63f85a77b5f90cdece497ba
Ashot Karapetyan <ashot.karapetyan.y@gmail.com>
2023-06-14 09:58:37 +0400

    Add knex and knex config

---
+++ src/config/knex-config.js
@@ -0,0 +1,12 @@
+import knex from 'knex';
+
+export const knexConfig = knex({
+  client: 'postgresql',
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
