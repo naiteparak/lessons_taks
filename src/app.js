1b892b3194342694a83755b1d129e3e4e6ff9f3b
Ashot Karapetyan <ashot.karapetyan.y@gmail.com>
2023-06-16 10:04:08 +0400

    Add documentation for API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
-import { appRouter } from './src/router/app-router.js';
+import { appRouter } from './router/app-router.js';
 import pg from 'pg';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger/swagger.json' assert { type: 'json' };
 
 // Custom DATE parser for pg module, see https://github.com/brianc/node-postgres/issues/818
 pg.types.setTypeParser(1082, (value) => {
@@ -11,7 +13,10 @@ pg.types.setTypeParser(1082, (value) => {
 const port = process.env.PORT;
 const app = express();
 
-app.use(express.json()).use(appRouter);
+app
+  .use(express.json())
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(appRouter);
 
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
