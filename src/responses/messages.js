dec96619758eb6a71dab140548637be75e9a1bba
Ashot Karapetyan <ashot.karapetyan.y@gmail.com>
2023-06-16 14:23:26 +0400

    Change error message

--- src/responses/messages.js
+++ src/responses/messages.js
@@ -7,5 +7,6 @@ export const ERROR_MESSAGES = {
   LESSON_COUNT_AND_LAST_DATE_CONFLICT:
     'Either lessons count or last date must be provided.',
   LESSON_COUNT: 'Lessons count can not exceed 300.',
-  INVALID_DATES_INTERVAL: 'Dates interval can not be greater than one year.',
+  INVALID_DATES_INTERVAL:
+    'Dates interval can not be greater than one year or less than one day.',
 };
