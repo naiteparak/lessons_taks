25b6d860af4324f525d8602754e5d285000eeaf1
Ashot Karapetyan <ashot.karapetyan.y@gmail.com>
2023-06-16 13:25:12 +0400

    Change folders names

--- src/responses/messages.js
+++ src/responses/messages.js
@@ -4,4 +4,8 @@ export const ERROR_MESSAGES = {
   INVALID_TEACHER_ID: 'Invalid teachers ids filter.',
   INVALID_STUDENTS_COUNT: 'Invalid students count filter.',
   INVALID_PAGINATION: 'Invalid page or lessonsPerPage filter.',
+  LESSON_COUNT_AND_LAST_DATE_CONFLICT:
+    'Either lessons count or last date must be provided.',
+  LESSON_COUNT: 'Lessons count can not exceed 300.',
+  INVALID_DATES_INTERVAL: 'Dates interval can not be greater than one year.',
 };
